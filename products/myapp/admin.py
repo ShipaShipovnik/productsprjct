2ad7b8212c2a7ea46ee19927307d0d6c9e380908
ShipaShipovnik <shipovnik2020d@gmail.com>
2024-01-12 22:19:43 +0300

    comit

--- products/myapp/admin.py
+++ products/myapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Orders,Customers, Products
+
+from .models import Orders, Customers, Products
 
 admin.site.register(Products)
 admin.site.register(Customers)
